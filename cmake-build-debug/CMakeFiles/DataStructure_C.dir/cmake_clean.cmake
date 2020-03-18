b3b684570b560a35ded3fcc465114b07d26adaf1
yymin1022 <yymin1022@gmail.com>
2020-03-18 17:11:26 +0900

    Initialized Commit

---
+++ cmake-build-debug/CMakeFiles/DataStructure_C.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/DataStructure_C.dir/main.c.o"
+  "DataStructure_C"
+  "DataStructure_C.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/DataStructure_C.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ main.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+
+int main() {
+    int nums[] = {9, 3, 4, 6, 2, 1, 7, 8, 5};
+
+    for(int i = 0; i < 8; i++){
+        int least = i;
+
+        for(int j = i + 1; j < 9; j++){
+            if(nums[j] < nums[least]){
+                least = j;
+            }
+        }
+
+        if(i != least){
+            int temp = nums[i];
+            nums[i] = nums[least];
+            nums[least] = temp;
+        }
+    }
+
+    printf("RESULT\n");
+
+    for(int i = 0; i < 9; i++){
+        printf("%d ", nums[i]);
+    }
+
+    return 0;
+}
