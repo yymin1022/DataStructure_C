6381971ee2425bc25c3aa15fb1e2f890dd08dc12
yymin1022 <yymin1022@gmail.com>
2020-04-01 19:56:39 +0900

    Update Program 1.7 Example Code

--- Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/cmake_clean.cmake
+++ Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/cmake_clean.cmake
@@ -1,5 +1,6 @@
 file(REMOVE_RECURSE
-  "CMakeFiles/DataStructure_C.dir/Assignment1_Program_1.7_a.c.o"
+  "CMakeFiles/DataStructure_C.dir/Assignment1_Problem1.7.c.o"
+  "CMakeFiles/DataStructure_C.dir/Assignment1_Problem_2_a.c.o"
   "DataStructure_C"
   "DataStructure_C.pdb"
 )
