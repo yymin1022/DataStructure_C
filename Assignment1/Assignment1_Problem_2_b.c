92909d5abd1eb578411376523180d29bd5476849
yymin1022 <yymin1022@gmail.com>
2020-04-02 01:35:55 +0900

    Add Problem 2 b

--- Assignment1/Assignment1_Problem_2_b.c
+++ Assignment1/Assignment1_Problem_2_b.c
@@ -2,11 +2,12 @@
 
 #define COMPARE(x, y) (((x) < (y)) ? -1: ((x) == (y))? 0: 1)
 
-int binsearch(int list[], int searchnum, int left, int right) {
+int binsearch(int *list, int searchnum, int left, int right, int length) {
     int middle;
+
     while (left <= right) {
         middle = (left + right) / 2;
-        switch (COMPARE(list[middle], searchnum)) {
+        switch (COMPARE(*(list + middle), searchnum)) {
             case -1:
                 left = middle + 1;
                 break;
@@ -16,5 +17,37 @@ int binsearch(int list[], int searchnum, int left, int right) {
                 right = middle - 1;
         }
     }
+
+    for(int i = 0; i < length; i++){
+        if(searchnum < *(list + i)){
+            int temp = *(list + i);
+            *(list + i) = searchnum;
+            for(int j = i + 1; j < length; j++){
+                int temp2;
+                if(j == i + 1){
+                    temp2 = *(list + j);
+                    *(list + j) = temp;
+                }else{
+                    int temp3 = *(list + j);
+                    *(list + j) = temp2;
+                    temp2 = temp3;
+                }
+            }
+            break;
+        }
+    }
+
     return -1;
+}
+
+int main(){
+    int list[7] = {1, 4, 9, 15, 30, 0, 0};
+    int listSize = sizeof(list) / sizeof(int);
+
+    printf("%d\n", binsearch(list, 30, 0, 4, listSize));
+    printf("%d\n", binsearch(list, 10, 0, 4, listSize));
+    printf("%d\n", binsearch(list, 10, 0, 5, listSize));
+    printf("%d\n", binsearch(list, 30, 0, 5, listSize));
+
+    return 0;
 }
--- Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/DependInfo.cmake
+++ Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/DependInfo.cmake
@@ -4,7 +4,7 @@ set(CMAKE_DEPENDS_LANGUAGES
   )
 # The set of files for implicit dependencies of each language:
 set(CMAKE_DEPENDS_CHECK_C
-  "/Volumes/Local Disk/AppProjects/DataStructure_C/Assignment1/Assignment1_Problem_2_a.c" "/Volumes/Local Disk/AppProjects/DataStructure_C/Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/Assignment1_Problem_2_a.c.o"
+  "/Volumes/Local Disk/AppProjects/DataStructure_C/Assignment1/Assignment1_Problem_2_b.c" "/Volumes/Local Disk/AppProjects/DataStructure_C/Assignment1/cmake-build-debug/CMakeFiles/DataStructure_C.dir/Assignment1_Problem_2_b.c.o"
   )
 set(CMAKE_C_COMPILER_ID "AppleClang")
 
