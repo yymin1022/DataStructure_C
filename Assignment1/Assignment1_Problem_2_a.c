b604c1c2886fa8f12dcc8c07459067cdef7a8505
yymin1022 <yymin1022@gmail.com>
2020-04-02 00:00:02 +0900

    Update Problem 2 a

--- Assignment1/Assignment1_Problem_2_a.c
+++ Assignment1/Assignment1_Problem_2_a.c
@@ -18,14 +18,3 @@ int binsearch(int list[], int searchnum, int left, int right) {
     }
     return -1;
 }
-
-int main(){
-//    int list[] = {1, 2, 5, 7, 12, 14, 16, 22, 29, 30};
-    int list[] = {30, 29, 22, 16, 14, 12, 7, 5, 2, 1};
-
-    int where22 = binsearch(list, 22, 9, 0);
-
-    printf("%d", where22);
-
-    return 0;
-}
