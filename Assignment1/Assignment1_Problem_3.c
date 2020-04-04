0705b23ec9cbcf2f95ca353f41b8a6d93fb48222
yymin1022 <yymin1022@gmail.com>
2020-04-04 17:57:18 +0900

    Add Multiply code of Problem 3

--- Assignment1/Assignment1_Problem_3.c
+++ Assignment1/Assignment1_Problem_3.c
@@ -7,10 +7,10 @@ typedef struct{
     int exp;
 } polynomial;
 
-int binSearch(int list[], int start, int end, int searchNum);
+int binSearch(polynomial poly[], int start, int end, int searchNum);
 
 int main(){
-    int A_coef[3] = {2, 3, 1}; int A_exp[3] = {2, 1, 0};
+    int A_coef[3] = {4, 3, 1}; int A_exp[3] = {2, 1, 0};
     int B_coef[3] = {1, 3, 2}; int B_exp[3] = {2, 1, 0};
 
     polynomial A[3];
@@ -25,30 +25,45 @@ int main(){
         B[i].exp = B_exp[i];
     }
 
-    printf("%d\n", binSearch((*A).exp, 0, 2, 2));
-
-    int list[3] = {3, 2, 1};
-
-    printf("%d\n", binSearch(list, 0, 2, 2));
-
-    for(int i = 0; i < 3; i++){
-        printf("%d", (*(A + i)).exp);
+    for(int i = 0; i < 9; i++){
+        C[i].coef = 0;
+        C[i].exp = 0;
     }
 
-//    printf("%d", binSearch((A.coef), 0, 2, 2));
+    for(int i = 0; i < 3; i++){
+        for(int j = 0; j < 3; j++){
+            int indexFromC = binSearch(C, 0, 8, A[i].exp + B[j].exp);
+
+            if(indexFromC != -1){
+                C[indexFromC].coef += A[i].coef * B[j].coef;
+            }else{
+                for(int k = 0; k < 9; k++){
+                    if(C[k].coef == 0 && C[k].exp == 0){
+                        C[k].coef += A[i].coef * B[j].coef;
+                        C[k].exp = A[i].exp + B[j].exp;
+                        break;
+                    }
+                }
+            }
+        }
+    }
+
+    for(int i = 0; i < 9; i++){
+        printf("%dx%d + ", C[i].coef, C[i].exp);
+    }
 
     return 0;
 }
 
-int binSearch(int list[], int start, int end, int searchNum){
+int binSearch(polynomial poly[], int start, int end, int searchNum) {
     int middle;
     
-    while(start >= end) {
+    while (start <= end) {
         middle = (start + end) / 2;
 
-        switch (COMPARE(list[middle], searchNum)) {
+        switch (COMPARE(poly[middle].exp, searchNum)) {
             case -1:
-                start= middle + 1;
+                start = middle + 1;
                 break;
             case 0:
                 return middle;
@@ -56,6 +71,5 @@ int binSearch(int list[], int start, int end, int searchNum){
                 end = middle - 1;
         }
     }
-
     return -1;
 }
