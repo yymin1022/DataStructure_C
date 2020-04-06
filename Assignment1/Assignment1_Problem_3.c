52c1f8ebf9c1afc57e51a51fae8ebfd42fb10e9b
yymin1022 <yymin1022@gmail.com>
2020-04-06 21:16:14 +0900

    Improve Efficiency for Problem 3

--- Assignment1/Assignment1_Problem_3.c
+++ Assignment1/Assignment1_Problem_3.c
@@ -12,8 +12,8 @@ int binSearch(polynomial poly[], int start, int end, int searchNum);
 void sortPolynomial(polynomial poly[], int length);
 
 int main(){
-    int A_coef[3] = {3, 4, 1}; int A_exp[3] = {1, 2, 0};
-    int B_coef[3] = {1, 3, 2}; int B_exp[3] = {2, 1, 0};
+    int A_coef[3] = {2, 2, 3}; int A_exp[3] = {3, 2, 4};
+    int B_coef[3] = {7, 3, 4}; int B_exp[3] = {0, 1, 2};
 
     polynomial A[3];
     polynomial B[3];
@@ -50,9 +50,10 @@ int main(){
                     }
                 }
             }
-            sortPolynomial(C, 9);
         }
     }
+
+    sortPolynomial(C, 9);
 
     for(int i = 0; i < 9; i++){
         int curCoef = C[i].coef;
