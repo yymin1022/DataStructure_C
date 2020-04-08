f57c40a1c19e03cc7dee9d624d58a576c47b2454
yymin1022 <yymin1022@gmail.com>
2020-04-09 02:06:19 +0900

    Ready for Assignment2

---
+++ Assignment2/cmake-build-debug/CMakeFiles/Assignment2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment2"
+  "Assignment2.pdb"
+  "CMakeFiles/Assignment2.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
