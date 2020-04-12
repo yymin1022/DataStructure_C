5ea8399b4e2bceee87bed0f9fc6d920029fff304
yymin1022 <yymin1022@gmail.com>
2020-04-12 16:56:42 +0900

    Update Problem 3 a base code

--- Assignment2/Assignment2_Problem_3_a.c
+++ Assignment2/Assignment2_Problem_3_a.c
@@ -1,6 +1,20 @@
 #include <stdio.h>
 
+void print1(int*, int);
+
 int main() {
     printf("Hello, World!\n");
     return 0;
+}
+
+void print1(int *ptr, int rows){
+    int i;
+
+    printf("Address Contents\n");
+
+    for(i = 0; i < rows; i++){
+        printf("%8u%5d\n", ptr + i, *(ptr + i));
+    }
+
+    printf("\n");
 }
