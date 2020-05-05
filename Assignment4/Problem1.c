f3f3708b72df607ea0ea8ab6b482848fa18cb355
yymin1022 <yymin1022@gmail.com>
2020-05-05 11:49:57 +0900

    Update Printing Algorithm

--- Assignment4/Problem1.c
+++ Assignment4/Problem1.c
@@ -10,7 +10,6 @@ typedef struct{
 } polynomial;
 
 polynomial padd(polynomial, polynomial);
-void printPolynomial(polynomial, int*);
 
 float A_coef[A_nonzero_terms] = {2.0, 2.0, 3.0};
 int A_exp[A_nonzero_terms] = {7, 3, 2};
@@ -21,39 +20,56 @@ int B_exp[B_nonzero_terms] = {5, 3, 1, 0};
 int main(){
     polynomial A, B, D;
 
-    A.degree = A_nonzero_terms;
+    A.degree = A_exp[0];
     for(int i = 0; i < A_nonzero_terms; i++){
         A.coef[i] = A_coef[i];
     }
 
-    B.degree = B_nonzero_terms;
+    B.degree = B_exp[0];
     for(int i = 0; i < B_nonzero_terms; i++){
         B.coef[i] = B_coef[i];
     }
 
     D = padd(A, B);
 
-    printPolynomial(A, A_exp);
-    printPolynomial(B, B_exp);
-//    printPolynomial(D)
+    printf("C(x) = ");
+    for (int i = 0; i <= D.degree; i++) {
+        if(D.coef[i] != 0){
+            printf("%5.1fx^%d ", D.coef[i], i);
+            if (i != D.degree) {
+                printf("+");
+            }
+        }
+    }
 
     return 0;
 }
 
 polynomial padd(polynomial A, polynomial B){
-    polynomial C = A;
+    polynomial C;
 
+    C.degree = (A.degree > B.degree) ? A.degree : B.degree;
 
+    int A_index = 0;
+    int B_index = 0;
+    int C_index = 0;
 
-    return C;
-}
+    int A_degree = A.degree;
+    int B_degree = B.degree;
 
-void printPolynomial(polynomial poly, int *exp){
-    printf("%.1fx^%d", poly.coef[0], *(exp));
-
-    for(int i = 1; i < poly.degree; i++){
-        printf(" + %.1fx^%d", poly.coef[i], *(exp + i));
+    while(A_index <= A.degree && B_index <= B.degree){
+        if(A_degree > B_degree){
+            C.coef[C_index++] = A.coef[A_index++];
+            A_degree--;
+        }else if(A_degree == B_degree){
+            C.coef[C_index++] = A.coef[A_index++] + B.coef[B_index++];
+            A_degree--;
+            B_degree--;
+        }else{
+            C.coef[C_index++] = B.coef[B_index++];
+            B_degree--;
+        }
     }
 
-    printf("\n");
+    return C;
 }
