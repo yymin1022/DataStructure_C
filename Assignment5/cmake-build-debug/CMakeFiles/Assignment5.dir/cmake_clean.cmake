c5c82869b84a67d09601b424abcaa35ba638acbb
yymin1022 <yymin1022@gmail.com>
2020-05-24 17:58:39 +0900

    Ready for Assignment5

---
+++ Assignment5/cmake-build-debug/CMakeFiles/Assignment5.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "Assignment5"
+  "Assignment5.pdb"
+  "CMakeFiles/Assignment5.dir/main.c.o"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment5.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
