4a39aab8979650651a0735899d75ca25ef3ea825
yymin1022 <yymin1022@gmail.com>
2020-05-28 01:07:26 +0900

    Update Assignment5 Problem1 : Update Lists which has 1 and empty node.

--- Assignment5/Problem1.c
+++ Assignment5/Problem1.c
@@ -11,6 +11,8 @@ listPointer invert(listPointer);
 void printList(listPointer);
 
 int main(){
+    /* Inverting of Linked List which has 2 nodes */
+    printf("Inverting of Linked List which has 2 nodes\n");
     struct listNode* L1 = (listPointer*)malloc(sizeof(listPointer));
     L1 -> link = NULL;
     L1 -> data = 10;
@@ -23,10 +25,37 @@ int main(){
     printf("Linked List before Invert\n");
     printList(L1);
 
-    struct listNode* invertList = invert(L1);
+    struct listNode* invertList1 = invert(L1);
 
     printf("Linked List after Invert\n");
-    printList(invertList);
+    printList(invertList1);
+
+    /* Inverting of Linked List which has 1 nodes */
+    printf("\nInverting of Linked List which has 1 nodes\n");
+    struct listNode* L3 = (listPointer*)malloc(sizeof(listPointer));
+    L3 -> link = NULL;
+    L3 -> data = 10;
+
+    printf("Linked List before Invert\n");
+    printList(L3);
+
+    struct listNode* invertList2 = invert(L3);
+
+    printf("Linked List after Invert\n");
+    printList(invertList2);
+
+    /* Inverting of Linked List which has Empty nodes */
+    printf("\nInverting of Linked List which has Empty nodes\n");
+    struct listNode* L4 = (listPointer*)malloc(sizeof(listPointer));
+    L4 -> link = NULL;
+
+    printf("Linked List before Invert\n");
+    printList(L4);
+
+    struct listNode* invertList3 = invert(L4);
+
+    printf("Linked List after Invert\n");
+    printList(invertList3);
 
     return 0;
 }
