ecc5140023a3b4ae0190ab3974072d362d36cf14
yymin1022 <yymin1022@gmail.com>
2020-05-28 05:15:34 +0900

    Update Assignment5 Problem2 : Added 2-Node Circular Linked List

--- Assignment5/Problem2.c
+++ Assignment5/Problem2.c
@@ -11,7 +11,23 @@ listPointer cinvert(listPointer);
 void printList(listPointer);
 
 int main() {
-    printf("Hello, World!\n");
+    struct listNode* CL2_0 = (listPointer*)malloc(sizeof(listPointer));
+    CL2_0 -> link = CL2_0;
+    CL2_0 -> data = 10;
+
+    struct listNode* CL2_1 = (listPointer*)malloc(sizeof(listPointer));
+    CL2_1 -> link = CL2_0 -> link;
+    CL2_1 -> data = 20;
+    CL2_0 -> link = CL2_1;
+
+    printf("Circular Linked List before Invert\n");
+    printList(CL2_0);
+
+    struct listNode* invertCL2 = cinvert(CL2_0);
+
+    printf("Circular Linked List after Invert");
+    printList(CL2_0);
+
     return 0;
 }
 
