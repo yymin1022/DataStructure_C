d80acdfc03866da30cee62d8229c3589f34e50f2
yymin1022 <yymin1022@gmail.com>
2020-05-28 17:53:53 +0900

    Update Assignment5 Problem2 : Fix Error on Printing and Inverting Process of List which has 1 or Empty node

--- Assignment5/Problem2.c
+++ Assignment5/Problem2.c
@@ -11,6 +11,8 @@ listPointer cinvert(listPointer);
 void printList(listPointer);
 
 int main() {
+    /* Inverting of Circular Linked List which has 2 nodes */
+    printf("Inverting of Circular Linked List which has 2 nodes\n");
     struct listNode* CL2_0 = (listPointer*)malloc(sizeof(listPointer));
     CL2_0 -> link = CL2_0;
     CL2_0 -> data = 10;
@@ -28,13 +30,45 @@ int main() {
     printf("Circular Linked List after Invert\n");
     printList(invertCL2);
 
+    /* Inverting of Circular Linked List which has 1 node */
+    printf("\nInverting of Circular Linked List which has 1 node\n");
+    struct listNode* CL1 = (listPointer*)malloc(sizeof(listPointer));
+    CL1 -> link = CL1;
+    CL1 -> link = CL1;
+    CL1 -> data = 10;
+
+    printf("Circular Linked List before Invert\n");
+    printList(CL1);
+
+    struct listNode* invertCL1 = cinvert(CL1);
+
+    printf("Circular Linked List after Invert\n");
+    printList(invertCL1);
+
+    /* Inverting of Linked List which has Empty node */
+    printf("\nInverting of Circular Linked List which has Empty node\n");
+    struct listNode* CL0 = (listPointer*)malloc(sizeof(listPointer));
+    CL0 -> link = NULL;
+
+    printf("Circular Linked List before Invert\n");
+    printList(CL0);
+
+    struct listNode* invertCL0 = cinvert(CL0);
+
+    printf("Circular Linked List after Invert\n");
+    printList(invertCL0);
+
     return 0;
 }
 
 listPointer cinvert(listPointer lead){
+    if(lead -> link == lead || lead -> link == NULL){
+        return lead;
+    }
+
     listPointer middle, trail;
     listPointer startPoint = lead;
-    middle = lead->link;
+    middle = lead -> link;
 
     int count = 0;
 
@@ -53,11 +87,13 @@ listPointer cinvert(listPointer lead){
 void printList(listPointer pointer){
     listPointer startPoint = pointer;
 
-    printf("Address : %p, Data : %d, Link : %p\n", pointer, pointer -> data, pointer->link);
+    printf("Address : %p, Data : %d, Link : %p\n", pointer, pointer -> data, pointer -> link);
     pointer = pointer -> link;
 
-    while(pointer != startPoint){
-        printf("Address : %p, Data : %d, Link : %p\n", pointer, pointer -> data, pointer->link);
-        pointer = pointer -> link;
+    if(pointer != NULL){
+        while(pointer != startPoint){
+            printf("Address : %p, Data : %d, Link : %p\n", pointer, pointer -> data, pointer -> link);
+            pointer = pointer -> link;
+        }
     }
 }
