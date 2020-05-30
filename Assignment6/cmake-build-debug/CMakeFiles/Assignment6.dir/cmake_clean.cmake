ff97e2a220bc06a519f1e077062c08ac8c5c46b6
yymin1022 <yymin1022@gmail.com>
2020-05-31 01:45:03 +0900

    Ready for Assignment 6

---
+++ Assignment6/cmake-build-debug/CMakeFiles/Assignment6.dir/cmake_clean.cmake
@@ -0,0 +1,12 @@
+file(REMOVE_RECURSE
+  "Assignment6.exe"
+  "Assignment6.exe.manifest"
+  "Assignment6.lib"
+  "Assignment6.pdb"
+  "CMakeFiles/Assignment6.dir/main.c.obj"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/Assignment6.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
