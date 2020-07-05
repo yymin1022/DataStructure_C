89a06c95e8c91784fcb4cdce254d9ec2556d445d
yymin1022 <yymin1022@gmail.com>
2020-07-06 00:00:13 +0900

    Update Assignment7 : Add Default Arrays

--- Assignment7_FinalExam/Problem1.c
+++ Assignment7_FinalExam/Problem1.c
@@ -1,5 +1,23 @@
 #include <stdio.h>
 
+#define MaxRow 5
+
+int bst[MaxRow][2] = {
+        {3, 17},
+        {1, 18},
+        {5, 20},
+        {4, 20},
+        {2, 15}
+};
+
+int mh[MaxRow][2] = {
+        {2, 20},
+        {1, 15},
+        {5, 20},
+        {1, 19},
+        {7, 17}
+};
+
 int main() {
     printf("Hello, World!\n");
     return 0;
